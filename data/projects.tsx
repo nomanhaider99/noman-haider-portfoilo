6d25e740b7a8e4d50c7f4ecb059fca86f5f013a2
Noman Haider <152289775+nomanhaider99@users.noreply.github.com>
2025-02-25 17:30:45 +0500

    errors resolved

--- data/projects.tsx
+++ data/projects.tsx
@@ -11,6 +11,6 @@ export const projectsData = [
         title: 'LMS Platform ',
         description: 'I built a car wash business website using Next.js, offering a responsive and user-friendly interface for booking services and contacting the business. The site is optimized for fast performance and seamless navigation across all devices.',
         icon: <FaYoutube size={70} color="#fff" />,
-        href: 'https://verse-blogging-website.vercel.app/'
+        href: 'https://lms-platform-nextjs-nu.vercel.app/'
     },
 ];
